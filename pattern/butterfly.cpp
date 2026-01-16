489f1d6aa1f1d5a553ef068cc96859830072195b
Shriyanshsoni96 <sonishriyansh9977@gmail.com>
2026-01-16 14:18:06 +0530

    buttrfly

--- pattern/butterfly.cpp
+++ pattern/butterfly.cpp
@@ -5,22 +5,49 @@ int main()
 int i ;
 cin>>i;
 for(int j=1 ; j<=i ; j++)
-{   cout<<endl;
-   for(int space =0 ; space < i - j ; space++){
-    cout<<" ";
+{ 
+    for(int k=1 ; k<=j ; k++)
+    {
+        cout<<" * ";
+    }
+    for(int space =0 ; space < i - j ; space++){
+    cout<<" - ";
+   }
+    for(int space =0 ; space < i - j ; space++){
+    cout<<" - ";
    }
     for(int k=1 ; k<=j ; k++)
     {
-        cout<<"*";
+        cout<<" * ";
     }
+      cout<<endl;
 }
 for(int j=i ; j>0 ; j--)
-{   cout<<endl;
+{   for(int k=1 ; k<=j ; k++)
+    {
+        cout<<" * ";
+    }
    for(int space =0 ; space < i - j ; space++){
-    cout<<"";
+    cout<<" - ";
+   }for(int space =0 ; space < i - j ; space++){
+    cout<<" - ";
    }
     for(int k=1 ; k<=j ; k++)
     {
-        cout<<"*";
-    }
+        cout<<" * ";
+    }cout<<endl;
+// for(int j=1 ; j<=i ; j++)
+// { 
+//     for(int s=1 ; s<=i-j+1 ; s++)
+//     {
+//         cout<<" - ";
+//     }
+//     for(int k=1 ; k<=j ; k++)
+//     {
+//         cout<<" * ";
+//     }
+//      cout<<endl;
+// }
+
+}
 }
--- pattern/rightang.cpp
+++ pattern/rightang.cpp
@@ -4,47 +4,19 @@ int main()
 {
 int i ;
 cin>>i;
+
+    
 for(int j=1 ; j<=i ; j++)
 { 
-    for(int k=1 ; k<=j ; k++)
-    {
-        cout<<" * ";
-    }
     for(int s=1 ; s<=i-j+1 ; s++)
     {
-        cout<<" - ";
-    }
-      for(int s=1 ; s<=i-j+1 ; s++)
-    {
-        cout<<" - ";
+        cout<<"  ";
     }
     for(int k=1 ; k<=j ; k++)
     {
         cout<<" * ";
     }
-      cout<<endl;
+     cout<<endl;
 }
-for(int j=i ; j>0 ; j--)
-{   cout<<endl;
-   for(int space =0 ; space < i - j ; space++){
-    cout<<" ";
-   }
-    for(int k=1 ; k<=j ; k++)
-    {
-        cout<<"*";
-    }
-// for(int j=1 ; j<=i ; j++)
-// { 
-//     for(int s=1 ; s<=i-j+1 ; s++)
-//     {
-//         cout<<" - ";
-//     }
-//     for(int k=1 ; k<=j ; k++)
-//     {
-//         cout<<" * ";
-//     }
-//      cout<<endl;
-// }
 
-}
 }
