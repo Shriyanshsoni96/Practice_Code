8cfd12ac53e9aaa6919483eaee9d9ed6ed398914
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-02-22 21:47:39 +0530

    leetcode

--- Leetcode/238.cpp
+++ Leetcode/238.cpp
@@ -1,19 +1,24 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
-int n[]={1,2,3,4};
-
- for(int i = 0 ; i<4;i++)
- {int pro=1;
-     for(int j = 0 ; j<4 ; j++)
+int n[]={1,2,3,4,5};
+ vector<int>answer;
+ for(int i = 0 ; i<5;i++)
+ {    int pro=1;
+     for(int j = 0 ; j<5 ; j++)
      {
         if(i!=j)
         {
             pro=pro*n[j];
         }
      }
-     cout<<pro<<" ";
+    
+   answer.push_back(pro);
  }
  
-return 0;
+for(int i = 0 ; i<answer.size();i++)
+{
+    cout<<answer[i]<<" ";
+}
 }
