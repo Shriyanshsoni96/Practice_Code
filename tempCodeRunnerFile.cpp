65cd71616df4c0e8792d43129abe1842d263203e
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-03-06 10:26:18 +0530

    findiing majority elemnet

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,19 +1,32 @@
-#include <iostream>
-#include<vector>
+#include<iostream>
+
 using namespace std;
-int main() {
-int n ;
+int main(){
+int n;
 cin>>n;
-vector<int>a(n);
-for(int j = 0 ; j<n ;j++)
+int a[n];
+if(1 <=  n >=(5*104))
 {
-    cin>>a[j];
+    for(int i = 0 ;i<n ;i++)
+    {
+    cin>>a[i];
+    }
+    int count=1;
+    for (int i = 0; i < n; i++)
+    { 
+    for(int j = i+1 ; j < n ; j++ )
+    {
+       if(a[i]==a[j])
+       {
+        count++;
+       }
+    }
+    if(count>n/2)
+    {
+        cout<<a[i];
+    }
+    count=0;
 }
-int sum=0 ;
-for(int i =0 ; i<n; i++)
-{
-    sum+=a[i];
-    cout<<sum<<" ";
 }
-return 0 ;
+return 0;
 }
