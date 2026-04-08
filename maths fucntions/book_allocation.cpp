e09178dd66c870e51d61e2664c3edfd399618686
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-04-08 17:46:51 +0530

    Book allocation probelm completed

--- maths fucntions/book_allocation.cpp
+++ maths fucntions/book_allocation.cpp
@@ -4,13 +4,52 @@
 #include<algorithm>
 using namespace std;
 
-int main()
-{   int m = 2; 
-    vector<int> arr ={10,20,30,40};
-    int s = *max_element(arr.begin(),arr.end());
-    int e =  accumulate(arr.begin(), arr.end(),0);
-    while(s<e)
-    {
-        
+bool isvalid(vector<int>& arr , int mid, int m )
+{
+    int student = 1;
+    int cur = 0;
+
+    for(int i : arr){
+        if(i > mid) return false;
+
+        if(cur + i <= mid)
+        {
+            cur += i;
+        }
+        else
+        {
+            student++;
+            cur = i;
+            if(student > m)
+                return false;
+        }
     }
+    return true;
+}
+
+int main()
+{   
+    int m = 2; 
+    vector<int> arr ={10,20,30,40};
+
+    int s = *max_element(arr.begin(),arr.end());
+    int e = accumulate(arr.begin(), arr.end(),0);
+
+    int ans = 0;
+
+    while(s <= e)
+    {
+        int mid = s + (e - s) / 2;
+
+        if(isvalid(arr, mid, m)) 
+        {
+            ans = mid;
+            e = mid - 1; 
+        }
+        else{
+            s = mid + 1;
+        }
+    }
+
+    cout << ans;
 }
