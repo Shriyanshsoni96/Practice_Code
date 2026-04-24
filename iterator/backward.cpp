493172a41d74616d7510f83f958f1385f2aa944e
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-04-24 14:39:37 +0530

    backward iterator in STL

---
+++ iterator/backward.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+vector<int> vec ={1,2,3,4,5,6};
+vector<int>::reverse_iterator it;
+for(it =vec.rbegin(); it!=vec.rend();it++)
+{
+    cout<<*it<<" ";
+}
+cout<<endl;
+return 0;
+}
