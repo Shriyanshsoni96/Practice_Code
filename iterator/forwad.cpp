515deb5843c3edb09eec2bae166b9f5dda4c4c01
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-04-24 14:39:48 +0530

    Stl

--- iterator/forwad.cpp
+++ iterator/forwad.cpp
@@ -2,7 +2,6 @@
 #include<vector>
 using namespace std;
 int main(){
- 
 vector<int> vec ={1,2,3,4,5,6};
 vector<int>::iterator it;
 for(it =vec.begin(); it!=vec.end();it++)
@@ -10,7 +9,5 @@ for(it =vec.begin(); it!=vec.end();it++)
     cout<<*it<<" ";
 }
 cout<<endl;
-
-
 return 0;
 }
